9d0db9c0f1f3c12f17d2cd83fd4b814bc1d3bae8
Your Name <mohnurulhaq559@gmail.com>
2026-06-16 21:23:10 +0700

    Add Railway start command

--- run.py
+++ run.py
@@ -12,7 +12,7 @@ app = create_app()
 if __name__ == "__main__":
     # Read host and port from environment variables (with sane defaults)
     host  = os.environ.get("FLASK_RUN_HOST", "127.0.0.1")
-    port  = int(os.environ.get("FLASK_RUN_PORT", 5000))
+    port  = int(os.environ.get("PORT", os.environ.get("FLASK_RUN_PORT", 5000)))
     debug = os.environ.get("FLASK_DEBUG", "True").lower() in ("true", "1", "yes")
 
     print(f"\n  * Veltrix is running -> http://{host}:{port}\n")
